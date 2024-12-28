ef5de4dde825612a93f6898d239e6e43c17ca26f
shayanbom <xjy204@gmail.com>
2024-12-28 23:53:32 +0800

    Add context of about penang page

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,7 @@ const About = () => {
             />
             <div className='aboutText'>
                 <h1>About Penang</h1>
-                <p>This is the About page. Content will be added later.</p>
+                <p>Penang is a popular destination in Malaysia known for its rich culture, history, and beautiful landscapes. It offers a variety of accommodations, from luxurious hotels to affordable stays. Getting around is easy with options like taxis, buses, and ride-hailing services. The island enjoys a tropical climate, with warm temperatures year-round, and experiences a rainy season from May to October.</p>
             </div>
         </div>
     );

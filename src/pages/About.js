85d900094bec813aa82c75990188b657d4cdece4
shayanbom <xjy204@gmail.com>
2024-12-28 23:00:39 +0800

    About penang image, moving text to side of picture

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>This is the About page. Content will be added later.</p>
+        <div className='about'>
+            <img
+                className="about-png-image"
+                src="/about-Png.jpg"
+                alt="Penang Bridge"
+            />
+            <div className='aboutText'>
+                <h1>About Penang</h1>
+                <p>This is the About page. Content will be added later.</p>
+            </div>
         </div>
     );
 };

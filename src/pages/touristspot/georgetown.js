d9cee5fbd46a278e5b9077b2a0b64063ff2f4139
junhao <junhaokhor04@gmail.com>
2024-12-29 21:39:44 +0800

    image carousel is added in Georgetown page

--- src/pages/touristspot/georgetown.js
+++ src/pages/touristspot/georgetown.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './georgetown.css';
 
 const Georgetown = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const images = [
+        { id: 1, src: '/kekloksitemple2.jpeg', alt: 'Kek Lok Si Temple', caption: 'Kek Lok Si Temple at Night' },
+        { id: 2, src: '/streetart2.jpg', alt: 'Street Art', caption: 'Iconic Street Art of Georgetown' },
+        { id: 3, src: '/cheongfatttzemansion.jpg', alt: 'Cheong Fatt Tze Mansion', caption: 'Cheong Fatt Tze Mansion' },
+    ];
+
+    // Handle navigation
+    const handleNavigation = (direction) => {
+        setCurrentIndex((prevIndex) =>
+            direction === 'next'
+                ? (prevIndex + 1) % images.length
+                : (prevIndex - 1 + images.length) % images.length
+        );
+    };
+
     return (
-        <div className="georgetown-page">
-            <h1>Welcome to Georgetown</h1>
-            <p>Here is some more detailed content about Georgetown.</p>
+        <div className="form-container">
+            {/* Image Display */}
+            <div className="image-container">
+                <img
+                    src={images[currentIndex].src}
+                    alt={images[currentIndex].alt}
+                    className="form-image"
+                />
+                <div className="image-caption">{images[currentIndex].caption}</div>
+                {/* Navigation Buttons */}
+                <button
+                    className="nav-button prev-button"
+                    onClick={() => handleNavigation('prev')}
+                >
+                    &#9664; {/* Left triangle arrow */}
+                </button>
+                <button
+                    className="nav-button next-button"
+                    onClick={() => handleNavigation('next')}
+                >
+                    &#9654; {/* Right triangle arrow */}
+                </button>
+            </div>
         </div>
     );
 };

eed973ea3327d50628840da0a3d666f89b7f4fcd
shayanbom <xjy204@gmail.com>
2024-12-29 14:28:42 +0800

    add a big div section in about.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,16 +3,19 @@ import './About.css';
 
 const About = () => {
     return (
-        <div className='about'>
-            <img
-                className="about-png-image"
-                src="/about-Png.jpg"
-                alt="Penang Bridge"
-            />
-            <div className='aboutText'>
-                <h1>About Penang</h1>
-                <p>Penang is a popular destination in Malaysia known for its rich culture, history, and beautiful landscapes. It offers a variety of accommodations, from luxurious hotels to affordable stays. Getting around is easy with options like taxis, buses, and ride-hailing services. The island enjoys a tropical climate, with warm temperatures year-round, and experiences a rainy season from May to October.</p>
+        <div className="about-penang-page">
+            <div className='about'>
+                <img
+                    className="about-png-image"
+                    src="/about-Png.jpg"
+                    alt="Penang Bridge"
+                />
+                <div className='aboutText'>
+                    <h1>About Penang</h1>
+                    <p>Penang is a popular destination in Malaysia known for its rich culture, history, and beautiful landscapes. It offers a variety of accommodations, from luxurious hotels to affordable stays. Getting around is easy with options like taxis, buses, and ride-hailing services. The island enjoys a tropical climate, with warm temperatures year-round, and experiences a rainy season from May to October.</p>
+                </div>
             </div>
+
         </div>
     );
 };

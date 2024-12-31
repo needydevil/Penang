2e29e813c3b042a56d43da47617e6ed9532d2788
WeiQing_Cutie <weiqing4288@gmail.com>
2024-12-31 12:16:09 +0800

    Fix naming crashing and modify to card

--- src/pages/History.js
+++ src/pages/History.js
@@ -5,94 +5,114 @@ const History = () => {
     return (
         <>
             <section className="history-container">
-                <div className="container1">
+                <div className="history-container1">
                     <img 
                         src="/Street_art_1.png" 
                         alt="Penang Street Art 1" 
-                        className="container-image" 
+                        className="history-container-image" 
                     />
                 </div>
-                <div className="container2">
+                <div className="history-container2">
                     <img 
                         src="/Street_art_2.png" 
                         alt="Penang Street Art 2" 
-                        className="container-image" 
+                        className="history-container-image" 
                     />
-                    <div className="text-content">
-                        <h1 className="hero-title">
+                    <div className="history-text-content">
+                        <h1 className="history-hero-title">
                             <span>PENANG</span>
                             <br />
                             History and Culture
                         </h1>
-                        <p className="hero-description">
+                        <p className="history-hero-description">
                             Look deep into culture, and you will understand everything better.
                         </p>
-                        <button className="hero-button">
+                        <button className="history-hero-button">
                             Get Started
                         </button>
                     </div>
                 </div>
-                <div className="container3">
+                <div className="history-container3">
                     <img 
                         src="/Street_art_3.png" 
                         alt="Penang Street Art 3" 
-                        className="container-image" 
+                        className="history-container-image" 
                     />
                 </div>
             </section>
 
-            <section className="cards">
-                <article className="card card--1">
-                    <div className="card__img" style={{ backgroundImage: "/Street_art_1.png"}}></div>
-                    <a href="#" className="card_link">
-                        <div className="card__img--hover">
+            <section>
+                <div>
+                    <h2 className="history-cards-title">
+                        Explore Penang's Culinary Heritage
+                    </h2>
+                </div>
+            </section>
+
+            <section className="history-cards">
+                <article className="history-card history-card--1">
+                    <div className="history-card__img">
                         <img 
-                        src="/Street_art_1.png" 
-                        alt="Penang Street Art 1" 
-                        className="backgroundImage" 
-                    />                            
+                            src="/Street_art_1.png" 
+                            alt="Penang Street Art 1" 
+                        />                   
+                    </div>                   
+                    <a href="#" className="history-card_link">
+                        <div className="history-card__img--hover">
+                            <img 
+                                src="/Street_art_1.png" 
+                                alt="Penang Street Art 1" 
+                            />                            
                         </div>
                     </a>
-                    <div className="card__info">
-                        <span className="card__category">Chinese</span>
-                        <h3 className="card__title">Explore Chinese Cuisine</h3>
-                        <span className="card__by">by <a href="#" className="card__author" title="author">Jun Hao</a></span>
+                    <div className="history-card__info">
+                        <span className="history-card__category">Chinese</span>
+                        <h3 className="history-card__title">Explore Chinese Cuisine</h3>
+                        <span className="history-card__by">by <a href="#" className="history-card__author" title="author">Jun Hao</a></span>
                     </div>
                 </article>
 
-                <article className="card card--2">
-                    <div className="card__img" style={{ backgroundImage: "/Street_art_1.png" }}></div>
-                    <a href="#" className="card_link">
-                    <div className="card__img--hover">
+                <article className="history-card history-card--2">
+                    <div className="history-card__img">
                         <img 
-                        src="/Street_art_1.png" 
-                        alt="Penang Street Art 1" 
-                        className="backgroundImage" 
-                    />                            
+                            src="/Street_art_1.png" 
+                            alt="Penang Street Art 1" 
+                        />                   
+                    </div>
+                    <a href="#" className="history-card_link">
+                        <div className="history-card__img--hover">
+                            <img 
+                                src="/Street_art_1.png" 
+                                alt="Penang Street Art 1" 
+                            />                            
                         </div>
                     </a>
-                    <div className="card__info">
-                        <span className="card__category">Malay</span>
-                        <h3 className="card__title">Explore Malay Cuisine</h3>
-                        <span className="card__by">by <a href="#" className="card__author" title="author">Wei Qing</a></span>
+                    <div className="history-card__info">
+                        <span className="history-card__category">Malay</span>
+                        <h3 className="history-card__title">Explore Malay Cuisine</h3>
+                        <span className="history-card__by">by <a href="#" className="history-card__author" title="author">Wei Qing</a></span>
                     </div>
                 </article>
 
-                <article className="card card--3">
-                    <div className="card__img" style={{ backgroundImage: "/Street_art_1.png"}}></div>
-                    <a href="#" className="card_link">
-                    <div className="card__img--hover">
+                <article className="history-card history-card--3">
+                    <div className="history-card__img">
                         <img 
-                        src="/Street_art_1.png" 
-                        alt="Penang Street Art 1" 
-                        className="backgroundImage" 
-                    />                            
+                            src="/Street_art_1.png" 
+                            alt="Penang Street Art 1" 
+                        />                   
+                    </div>
+                    <a href="#" className="history-card_link">
+                        <div className="history-card__img--hover">
+                            <img 
+                                src="/Street_art_1.png" 
+                                alt="Penang Street Art 1" 
+                            />                            
                         </div>
                     </a>
-                    <div className="card__info">
-                        <span className="card__category">Indian</span>
-                        <h3 className="card__title">Explore Indian Cuisine</h3>
-                        <span className="card__by">by <a href="#" className="card__author" title="author">Xin Jing</a></span>
+                    <div className="history-card__info">
+                        <span className="history-card__category">Indian</span>
+                        <h3 className="history-card__title">Explore Indian Cuisine</h3>
+                        <span className="history-card__by">by <a href="#" className="history-card__author" title="author">Xin Jing</a></span>
                     </div>
                 </article>
             </section>

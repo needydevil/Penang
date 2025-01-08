56375d934b852b88619b68c397c916c95bea502c
jm <jiongming2004@gmail.com>
2025-01-08 13:25:57 +0800

    improve overall look of the food pages
    improve overall look of the food pages

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './FoodCard.css';
 
-const FoodCard = ({ image, name, description }) => {
+const FoodCard = ({ image, name, description, onClick }) => {
   return (
-    <div className="food-card">
+    <div className="food-card" onClick={onClick}>
       <img className="food-card-img" src={image} alt={name} />
       <div className="food-card-content">
         <h3 className="food-card-name">{name}</h3>

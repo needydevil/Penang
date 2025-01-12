d79d6005eec24a55d71d01dc220eee32791d0c24
jm <jiongming2004@gmail.com>
2025-01-12 13:35:51 +0800

    display 2 different video for mobile and desktop

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './home.css';
 
 const Home = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        handleResize(); // Check initial screen size
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const videoSrc = isMobile
+        ? "https://www.youtube.com/embed/L_PECjvzgsk?autoplay=1&mute=1&loop=1&playlist=L_PECjvzgsk"
+        : "https://www.youtube.com/embed/KeDSg4Y_njg?autoplay=1&mute=1&loop=1&playlist=KeDSg4Y_njg";
+
     return (
         <div className="home-page">
             {/* Full-screen Video */}
             <div className="video-container">
                 <iframe
                     className="background-video"
-                    src="https://www.youtube.com/embed/L_PECjvzgsk?autoplay=1&mute=1&loop=1&playlist=L_PECjvzgsk"
+                    src={videoSrc}
                     frameBorder="0"
                     allow="autoplay; encrypted-media"
                     allowFullScreen
